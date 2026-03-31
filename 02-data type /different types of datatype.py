74b44c2566d275102359fbe0c90bf59af62ee7ff
sarweshkumar86 <71916533+sarweshkumar86@users.noreply.github.com>
2026-03-31 12:30:42 +0000

    add files

---
+++ 02-data type /different types of datatype.py
@@ -0,0 +1,57 @@
+# Single Data Types (Single Value)
+
+# 🔢 1. Integer (int) – whole number
+age = 25
+print(age)
+print(type(age))
+
+
+# 🌡️ Float
+price = 99.99
+print(price)
+print(type(price))
+
+
+
+# 🔤 String
+name = "Sarwesh"
+print(name)
+print(type(name))
+
+
+
+# 🔘 Boolean
+is_pass = True
+print(is_pass)
+print(type(is_pass))
+
+
+
+# ......................................................................
+
+
+# 📋 List
+fruits = ["apple", "banana"]
+print(fruits)
+print(type(fruits))
+
+
+
+# 🔒 Tuple
+data = ("apple", "banana")
+print(data)
+print(type(data))
+
+
+
+# 🚫 Set
+numbers = {1, 2, 3}
+print(numbers)
+print(type(numbers))
+
+
+
+# 🔑 Dictionary
+student = {"name": "Govind"}
+print(student)
+print(type(student))

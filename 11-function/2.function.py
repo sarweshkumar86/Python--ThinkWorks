4ee5637da095c440389a1df8c361b4beb155fc21
sarweshkumar86 <71916533+sarweshkumar86@users.noreply.github.com>
2026-04-03 03:32:25 +0000

    files

--- 11-function/2.function.py
+++ 11-function/2.function.py
@@ -100,12 +100,3 @@ def show(n):
 show(1)   # 1 2 3 4 5
 
 
-# 🧪 Q9. Write a Python program to find factorial using recursion.
-
-
-def fact(n):
-        if n == 1:
-                return 1
-                    return n * fact(n - 1)
-                    
-                    print(fact(5))   # 120

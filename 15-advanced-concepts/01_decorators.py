a7caf94541641f05ed1d7c2a540b2e7fb9966ec3
sarweshkumar86 <71916533+sarweshkumar86@users.noreply.github.com>
2026-04-10 12:06:09 +0000

    files

---
+++ 15-advanced-concepts/01_decorators.py
@@ -0,0 +1,18 @@
+# Decorator is a function that takes a function, it creates a new function inside its body (wrapper). Then it returns that new function
+def decorator(func):
+    def wrapper():
+        print("I am about to execute a function....")
+        func()
+        print("I have executed this function....")
+    return wrapper   #return to   decorator() function 
+
+@decorator
+def say_hello():
+    print("Hello!")
+
+say_hello()
+
+
+
+
+
